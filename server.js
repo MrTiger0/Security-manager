0befcfd8347b3f2f98903ea7737c6960979b8563
hama120 <74502300+hama120@users.noreply.github.com>
2021-01-12 22:02:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1433,3 +1433,257 @@ badboy.react("👎")
   }
 })
 
+var top = require("./top.json");
+function save() {
+  fs.writeFileSync("./top.json", JSON.stringify(top, null, 4));
+}
+client.on("voiceStateUpdate", async function(oldMember, newMember) {
+  if (newMember.author.bot) return;
+  if (!top[newMember.guild.id]) top[newMember.guild.id] = {};
+  if (!top[newMember.guild.id][newMember.user.id])
+    top[newMember.guild.id][newMember.user.id] = {
+      text: 0,
+      voice: parseInt(Math.random() * 10),
+      msgs: 0,
+      id: newMember.user.id
+    };
+  save();
+  if (!oldMember.voiceChannel && newMember.voiceChannel) {
+    var addXP = setInterval(async function() {
+      top[newMember.guild.id][newMember.user.id].voice += parseInt(
+        Math.random() * 4
+      );
+      save();
+      if (!newMember.voiceChannel) {
+        clearInterval(addXP);
+      }
+    }, 60000);
+  }
+});
+client.on("message", async function(message) {
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!top[message.guild.id]) top[message.guild.id] = {};
+  if (!top[message.guild.id][message.author.id])
+    top[message.guild.id][message.author.id] = {
+      text: parseInt(Math.random() * 10),
+      voice: 1,
+      msgs: 0,
+      id: message.author.id
+    };
+  if (top[message.guild.id][message.author.id].msgs > 10) {
+    top[message.guild.id][message.author.id].text += parseInt(
+      Math.random() * 4
+    );
+    top[message.guild.id][message.author.id].msgs = 0;
+  }
+  save();
+  var args = message.content.split(" ");
+  var cmd = args[0].toLowerCase();
+  if (!message.content.startsWith(prefix)) return;
+  if (message.content.startsWith(prefix + "top text")) {
+    var topArray = Object.values(top[message.guild.id]);
+    var num = 0;
+    var textStr = `${topArray
+      .sort((a, b) => b.text - a.text)
+      .slice(0, 5)
+      .filter(user => user.text > 0 && message.guild.members.cache.get(user.id))
+      .map(function(user) {
+        if (user.text > 0) {
+          return `**#${++num} | <@${user.id}> XP: \`${user.text}\` **`;
+        }
+      })
+      .join("\n")}`;
+    var embed = new Discord.MessageEmbed()
+      .setAuthor("📑| Guild Score Leaderboards", message.guild.iconURL())
+      .setColor("13B813")
+      .addField(
+        `**:speech_balloon: | TEXT LEADERBOARD**`,
+        `${textStr}   \n\n\n **? |  More info: \`${prefix}top text\`**`,
+        true
+      )
+      .setFooter(message.author.tag, message.author.displayAvatarURL())
+      .setTimestamp();
+    message.channel.send({
+      embed: embed
+    });
+    //   if (!message.content.startsWith(prefix)) return;
+  } else {
+    if (message.content.startsWith(prefix + "top voice")) {
+      var topArray = Object.values(top[message.guild.id]);
+      var num = 0;
+      var voiceStr = `${topArray
+        .sort((a, b) => b.voice - a.voice)
+        .slice(0, 5)
+        .filter(
+          user => user.voice > 0 && message.guild.members.cache.get(user.id)
+        )
+        .map(function(user) {
+          if (user.voice > 0) {
+            return `**#${++num} | <@${user.id}> XP: \`${user.voice}\` **`;
+          }
+        })
+        .join("\n")}`;
+      var embed = new Discord.MessageEmbed()
+        .setAuthor("📑 | Guild Score Leaderboards", message.guild.iconURL())
+        .setColor("13B813")
+        .addField(
+          `**:microphone2: | VOICE LEADERBOARD**`,
+          `${voiceStr}   \n\n\n **:sparkles: More info ** \`${prefix}top voice\``,
+          true
+        )
+
+        .setFooter(message.author.tag, message.author.displayAvatarURL())
+        .setTimestamp();
+      message.channel.send({
+        embed: embed
+      });
+    } else {
+      if (message.content.startsWith(prefix + "reset voice")) {
+        if (!message.member.hasPermission("MANAGE_GUILD"))
+          return message.channel.send(`you don have MANAGE_SERVER`);
+        var reset = ":white_check_mark: successfully reset voice ";
+        var confirm = "YES:✅    NO:❎";
+
+        message.channel.send(`**${confirm}**`).then(async msg => {
+          await msg.react("✅");
+          await msg.react("❎");
+          const doma = msg.createReactionCollector(
+            (reaction, user) =>
+              reaction.emoji.name === "✅" && user.id === message.author.id,
+            { time: 60000 }
+          );
+          const ziad = msg.createReactionCollector(
+            (reaction, user) =>
+              reaction.emoji.name === "❎" && user.id === message.author.id,
+            { time: 60000 }
+          );
+          doma.on("collect", async r => {
+            msg.delete();
+
+            msg.channel.send(`${reset}`);
+          });
+
+          ziad.on("collect", async r => {
+            msg.delete();
+          });
+        });
+
+        // break;
+        if (!message.content.startsWith(prefix)) return;
+      } else {
+        if (message.content.startsWith(prefix + "top")) {
+          var topArray = Object.values(top[message.guild.id]);
+          var num = 0;
+          var textStr = `${topArray
+            .sort((a, b) => b.text - a.text)
+            .slice(0, 10)
+            .filter(
+              user => user.text > 0 && message.guild.members.cache.get(user.id)
+            )
+            .map(function(user) {
+              if (user.text > 0) {
+                return `**#${++num} | <@${user.id}> XP: \`${user.text}\` **`;
+              }
+            })
+            .join("\n")}`;
+          num = 0;
+          var voiceStr = `${topArray
+            .sort((a, b) => b.voice - a.voice)
+            .slice(0, 10)
+            .filter(
+              user => user.voice > 0 && message.guild.members.cache.get(user.id)
+            )
+            .map(function(user) {
+              if (user.voice > 0) {
+                return `**#${++num} | <@${user.id}> XP: \`${user.voice}\` **`;
+              }
+            })
+            .join("\n")}`;
+          var embed = new Discord.MessageEmbed()
+            .setAuthor("📑| Guild Score Leaderboards", message.guild.iconURL())
+            .addField(
+              "**TOP 5 TEXT :speech_balloon:**",
+              `${textStr}  \n\n  **:sparkles: More info** \`${prefix}top text\``,
+              true
+            )
+            .addField(
+              "**TOP 5 VOICE :microphone2:**",
+              `${voiceStr} \n\n **:sparkles: More info** \`${prefix}top voice\``,
+              true
+            )
+            .setFooter(message.author.tag, message.author.displayAvatarURL())
+            .setTimestamp()
+            .setColor("13B813");
+          message.channel.send({ embed: embed });
+        }
+      }
+    }
+  }
+});
+////////
+const rcrank = JSON.parse(fs.readFileSync("rcrank.json", "utf8")); 
+
+client.on('message', LORD => {
+
+if (!rcrank[LORD.author.id])
+ rcrank[LORD.author.id]={points:0,level:0,nextLvlXp:200};
+
+if (LORD.author.bot) return;
+
+rcrank[LORD.author.id].points = Math.floor(rcrank[LORD.author.id].points + 1); 
+
+if (rcrank[LORD.author.id].nextLvlXp <= rcrank[LORD.author.id].points) {
+
+rcrank[LORD.author.id].level = Math.floor(rcrank[LORD.author.id].level + 1);
+
+rcrank[LORD.author.id].nextLvlXp = Math.floor(rcrank[LORD.author.id].points * 20);
+
+LORD.channel.send(`**${LORD.author.username}, You leveld up to __${rcrank[LORD.author.id].level}__**`);
+
+};
+
+fs.writeFile('rcrank.json', JSON.stringify(rcrank), (err) => {  
+
+if (err) console.error(err);
+
+});
+
+})  // coded BY LORD
+
+client.on("message", async LORD => {
+
+if (LORD.author.bot) return; 
+
+if(!LORD.channel.guild) return;
+
+if (LORD.content.startsWith(prefix + "rank")) {
+
+var ment = LORD.mentions.users.first();
+
+var getvalueof;  
+
+if(ment) {getvalueof = ment;}else{getvalueof = LORD.author;}
+
+if(getvalueof.id === client.user.id) {return LORD.channel.send("😉 | I am on level 100")}  // coded by LORD
+
+if(getvalueof.bot) {return LORD.channel.send("Bot not have levels")}
+
+if (!rcrank[getvalueof.id]) rcrank[getvalueof.id] = {points: 0, level: 1,nextLvlXp:200};
+
+let embed = new MessageEmbed()  // coded by LORD
+
+.setColor("#ff2050")
+
+.setThumbnail(getvalueof.avatarURL())
+
+.setAuthor(getvalueof.username)  // coded by LORD
+
+.setDescription(`**LEVEL** - ${rcrank[getvalueof.id].level}
+**XP** - ${rcrank[getvalueof.id].points}/${rcrank[getvalueof.id].nextLvlXp}`)   // coded by LORD
+
+ LORD.channel.send(embed)
+
+}  
+
+});
