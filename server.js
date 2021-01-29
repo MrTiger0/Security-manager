91fa394ada1db46d1ca9731221c275713e518036
zallm00 <76389685+zallm00@users.noreply.github.com>
2021-01-29 12:58:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1316,40 +1316,6 @@ message.channel.send(tnx)
 
 //=================================[ say ]==================================//
 
-client.on("message", message => {
-  let commands = message.content.split(" ");
-  if (commands[0] == prefix + "say") {
-if (cooldown.has(message.author.id)) {
-      return message.channel.send(`⏱ | Please wait for 5 second`).then(m=>{m.delete({timeout:cdtime * 600})})
-    }
-
-    cooldown.add(message.author.id);
-
-    setTimeout(() => {
-      cooldown.delete(message.author.id);
-    }, cdtime * 1000);
-    if (!message.guild) return;
-    if (!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES"))
-      return message.reply("**You Dont Have `MANAGE_MESSAGES` Permission .**");
-    if (!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES"))
-      return message.reply(
-        "Please Check My Role Permission To `MANAGE_MESSAGES`"
-      );
-    var args = message.content
-      .split(" ")
-      .slice(1)
-      .join(" ");
-    if (!args) {
-      return message.channel.send("`Usage : " + prefix + "say <message>`");
-    }
-    message.delete();
-      .setColor("#808080")
-      .setDescription(`${args}`)
-      .setFooter(`By ${message.author.tag}`);
-    message.channel.send(embed);
-  }
-});
-
 //=================================[ warn ]=================================//
 
 client.on('message', message => {
