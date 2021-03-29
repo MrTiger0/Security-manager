a438a1f88b4fe6c8f5ed092258d7da593aff2641
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-03-29 17:49:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const dateFormat = require("dateformat");
 var table = require("table").table;
 const Discord = require("discord.js");
 const cmd = require("node-cmd");
-const prefix = "!";
+const prefix = "$";
 const cooldown = new Set()
 const cdtime =5;
 client.login("ODA0MDY4MDE3MDc1MjU3Mzc0.YBG8sg.vfpMtmEczFrNo70R-WCndSkWltU");
