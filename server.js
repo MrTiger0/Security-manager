403434fe6d2374230f7593baeaa7e651e30956b8
Dark Man <77631739+DarkMan9054@users.noreply.github.com>
2021-03-29 17:59:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ Hey My name is **Protection** and My Work is to Protection You
 \`${prefix}unban\`
 <a:3arz:823238395895349358> **Fun Commands**
 \`${prefix}ship\` - \`${prefix}gay\` - \`${prefix}iq\` - \`${prefix}old\`- \`${prefix}love\`
-<a:links:813729922643263558> Links
+<a:links:813729922643263558> **Links**
 [INVITE](https://discord.com/api/oauth2/authorize?client_id=804068017075257374&permissions=8&scope=bot) | [SUPPORT](https://discord.gg/BQDx5cNvXS)
 
 `);
